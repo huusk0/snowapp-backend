fe5a03ca1ba27fb25f5eeecfe188ef5190dcda7d
Tuomas Huusko <huusko.tuomas@gmail.com>
2026-01-25 19:06:59 +0200

    Initial sectoring functionality

--- scratch/guitest.py
+++ scratch/guitest.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 import math
 import copy
+from areatest import Area
+from snowSector import SnowSector
 
 pygame.init()
 
@@ -13,9 +15,9 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("test")
 
 # lumikola settings
-kola_width = 1
-kola_height = 1
-kola_capacity = 10
+kola_width = 10
+kola_height = 10
+kola_capacity = 100
 
 # Colors
 WHITE = (255, 255, 255)
@@ -28,14 +30,56 @@ BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 LIGHT_GREEN = (144, 238, 144)
-
 # Fonts
 font = pygame.font.SysFont(None, 24)
-
-
 # Button setup
 button_rect = pygame.Rect(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT - 60, 100, 40)
 prev_pos = []
+
+scale = 3
+x_offset = 10
+y_offset = 10
+
+areas = [
+    Area(40 * scale, 30 * scale, 10 * scale, (100 * scale, 10 * scale)),
+    Area(60 * scale, 50 * scale, 10 * scale, (80 * scale, 50 * scale)),
+    Area(40 * scale, 20 * scale, 10 * scale, (80 * scale, 110 * scale)),
+    Area(20 * scale, 20 * scale, 10 * scale, (80 * scale, 150 * scale)),
+    Area(20 * scale, 40 * scale, 10 * scale, (40 * scale, 150 * scale)),
+    Area(10 * scale, 20 * scale, 10 * scale, (20 * scale, 160 * scale)),
+    Area(30 * scale, 20 * scale, 10 * scale, (40 * scale, 170 * scale)),
+]
+
+snow_sectors = [SnowSector((10, 10), 100, "RED")]
+
+
+def draw_areas():
+    for area in areas:
+        pygame.draw.rect(
+            screen,
+            LIGHT_GRAY,
+            (*area.coords, area.width, area.height),
+        )
+
+
+def draw_snow_sectors():
+    for sector in snow_sectors:
+        pygame.draw.circle(screen, sector.color, sector.coords, 1 * scale)
+
+
+def split_to_sectors():
+    for area in areas[0:2]:
+        for i in range(0, math.ceil(area.width / kola_width)):
+            for j in range(0, math.ceil(area.height / kola_height)):
+                snow_sector = SnowSector(
+                    (
+                        area.coords[0] + x_offset * (i + 1),
+                        area.coords[1] + y_offset * (j + 1),
+                    ),
+                    area.height * area.width * area.snow_depth,
+                    "BLUE",
+                )
+                snow_sectors.append(snow_sector)
 
 
 def main():
@@ -46,6 +90,9 @@ def main():
     pygame.draw.rect(screen, BLUE, button_rect)
     text = font.render("split", True, WHITE)
     screen.blit(text, (button_rect.x + 25, button_rect.y + 10))
+    split_to_sectors()
+    draw_areas()
+    draw_snow_sectors()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:

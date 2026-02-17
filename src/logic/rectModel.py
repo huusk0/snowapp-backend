c1f40761693bc259478bef06eb50e4f3f14250d8
Tuomas Huusko <huusko.tuomas@gmail.com>
2026-02-17 12:41:44 +0200

    FEAT: add functions for finding tsp-path from given rectangles

--- src/logic/rectModel.py
+++ src/logic/rectModel.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 import math
+import networkx as nx
 
 
 class Rectangle:
@@ -88,3 +90,69 @@ class GeometryService:
                     )
 
         return snow_sectors
+
+    def snow_sectors_to_coords(
+        self, snow_sectors: list[SnowSector]
+    ) -> list[tuple[int, int]]:
+        coords = []
+        for sector in snow_sectors:
+            coords.append((sector.coords[0], sector.coords[1]))
+        print("COORDS: ", coords)
+        return coords
+
+    def generate_edges(
+        self,
+        snow_sectors: list[SnowSector],
+        kola_width: int = 10,
+        kola_height: int = 10,
+    ) -> list[tuple[tuple[int, int], tuple[int, int]]]:
+        cols = defaultdict(list)
+        rows = defaultdict(list)
+
+        for sector in snow_sectors:
+            x = sector.coords[0]
+            y = sector.coords[1]
+            cols[x].append(y)
+            rows[y].append(x)
+
+        for x in cols:
+            cols[x].sort()  # sort y's in the same column
+        for y in rows:
+            rows[y].sort()  # sort x's in the same row
+
+        edges = []
+
+        # Connect along columns
+        for x, y_list in cols.items():
+            for i in range(len(y_list) - 1):
+                if y_list[i + 1] - y_list[i] <= kola_width:
+                    edges.append(((x, y_list[i]), (x, y_list[i + 1])))
+
+        # Connect along rows
+        for y, x_list in rows.items():
+            for i in range(len(x_list) - 1):
+                if x_list[i + 1] - x_list[i] <= kola_height:
+                    edges.append(((x_list[i], y), (x_list[i + 1], y)))
+        return edges
+
+    def find_path_v0(
+        self,
+        areas: list[Rectangle],
+        start_node: tuple[int, int] | None,
+        kola_width: int = 10,
+        kola_height: int = 10,
+    ) -> list[tuple[int, int]]:
+        snow_sectors = self.split_to_sectors(areas)
+        coords = self.snow_sectors_to_coords(snow_sectors)
+        edges = self.generate_edges(snow_sectors)
+        G = nx.Graph()
+        G.add_nodes_from(coords)
+        G.add_edges_from(edges)
+        tsp_path = nx.approximation.traveling_salesman_problem(G, cycle=True)
+        if start_node is None:
+            start_node = coords[0]
+        if start_node in tsp_path:
+            idx = tsp_path.index(start_node)
+            tsp_path = tsp_path[idx:] + tsp_path[:idx]
+
+        return tsp_path
